7ce279dbcdf5bd56f37fd30a7581e0806f6e62df
Tamer Ahmed <44730055+TamerAbdelatyAhmed@users.noreply.github.com>
2019-10-03 01:40:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,27 @@
-from flask import Flask, render_template, url_for, flash, redirect, request
+#Import dependencies
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-from send_email import send_email
-import smtplib
+#Create instance of Flask App
+app = Flask(__name__)
 
-app=Flask(__name__)
-
-#app.config['SQLALCHEMY_DATABASE_URI']="sqlite:////C:/datana/data.db"
-#app.config['SQLALCHEMY_DATABASE_URI']='postgresql://postgres:postgres123@localhost/data'
-app.config['SQLALCHEMY_DATABASE_URI']="postgres://bhanumdxiqwwmb:c158369ee7766967b67a7fa36d4531aa8e0aa0fccf93042e24a8ad2e9a8ff320@ec2-107-21-201-238.compute-1.amazonaws.com:5432/d9bknqpmb4athe"
+#Connect to the Database
+app.config['SQLALCHEMY_DATABASE_URI']="postgres://ypenmacgwpxmfm:0e3d4ca190f9742e0f25a56c2a51d9075e3cf28bb3e0c2e44128f52f180bb69a@ec2-107-20-167-241.compute-1.amazonaws.com:5432/ddq8054gd9prrv"
 db=SQLAlchemy(app)
 
 class Data(db.Model):
     __tablename__="data"
-    Name=db.Column(db.String(20), primary_key=True)
-    index=db.Column(db.Integer(), unique=True)
-    Email=db.Column(db.String(120), unique=True)
-    Date=db.Column(db.Integer(), unique=True)
-    Blood_Pressure=db.Column(db.String(120), unique=True)
-    Blood_Glucose=db.Column(db.String(120), unique=True)
-    Weight=db.Column(db.Integer(), unique=True)
-    Height=db.Column(db.String(120), unique=True)
-    Body_Temp=db.Column(db.Integer(), unique=True)
-    Medications_History=db.Column(db.String(120), unique=True)
-    Diseases_History=db.Column(db.String(120), unique=True)
-    Notes=db.Column(db.String(120), unique=True)
+    Name=db.Column(db.String, primary_key=True)
+    index=db.Column(db.Integer)
+    Email=db.Column(db.String) 
+    Date=db.Column(db.Integer)
+    Blood_Pressure=db.Column(db.String)
+    Blood_Glucose=db.Column(db.String)
+    Weight=db.Column(db.Integer)
+    Height=db.Column(db.String)
+    Body_Temp=db.Column(db.Integer)
+    Medications_History=db.Column(db.String)
+    Diseases_History=db.Column(db.String)
+    Notes=db.Column(db.String)
 
     def __init__(self, Name, index, Email, Date, Blood_Pressure, Blood_Glucose, Weight, Height, Body_Temp, Medications_History, Diseases_History, Notes):
         self.Name = Name
@@ -39,30 +37,31 @@ class Data(db.Model):
         self.Diseases_History=Diseases_History
         self.Notes=Notes
 
-Name = Data.Name
-Email = Data.Email
-Date = Data.Date
-Blood_Pressure = Data.Blood_Pressure
-Blood_Glucose = Data.Blood_Glucose
-Weight = Data.Weight
-Height = Data.Height
-Body_Temp = Data.Body_Temp
-Medications_History = Data.Medications_History
-Diseases_History = Data.Diseases_History
-Notes = Data.Notes
+
+#Define Route and Contant of that page
 @app.route("/")
 def index():
     return render_template("index.html")
-    send_email(subject, msg)
 
 @app.route("/success", methods=['POST'])
 def success():
-    Email=request.form["Email"]
-    Height=request.form["Height"]
-    print(Email, Height)
-    data=Data(Name, index, Email, Date, Blood_Pressure, Blood_Glucose, Weight, Height, Body_Temp, Medications_History, Diseases_History, Notes)
-    db.session.add(data)
-    return render_template("success.html")
+    if(request.method == 'POST'):
+        Name_=request.form["Name"]
+        index_=request.form["index"]
+        Email_=request.form["Email"]
+        Date_=request.form["Date"]
+        Blood_Pressure_=request.form["Blood_Pressure"]
+        Blood_Glucose_=request.form["Blood_Glucose"]
+        Weight_=request.form["Weight"]
+        Height_=request.form["Height"]
+        Body_Temp_=request.form["Body_Temp"]
+        Medications_History_=request.form["Medications_History"]
+        Diseases_History_=request.form["Diseases_History"]
+        Notes_=request.form["Notes"]
+        data=Data(Name_, index_, Email_, Date_, Blood_Pressure_, Blood_Glucose_, Weight_, Height_, Body_Temp_, Medications_History_, Diseases_History_, Notes_)
+        db.session.add(data)
+        db.session.commit()
+        return render_template("success.html")
 if __name__ == '__main__':
     app.debug=True
-    app.run(port=5005)
+    
